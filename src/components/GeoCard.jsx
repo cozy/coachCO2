7d890754f3d404e51a0b1850f7e4888263de4df9
JF-Cozy <julien.ficheux@cozycloud.cc>
2021-12-15 15:41:51 +0100

    fix: Revert to classic OSM map style to avoid CSP problem

--- src/components/GeoCard.jsx
+++ src/components/GeoCard.jsx
@@ -18,13 +18,10 @@ const setupMap = node => {
 
   const map = L.map(node).setView([51.505, -0.09], 13)
 
-  L.tileLayer(
-    'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
-    {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-    }
-  ).addTo(map)
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map)
 
   return map
 }
