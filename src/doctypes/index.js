b6fc0ace5da68b10701ccf505cdf913f45658455
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-03-08 16:15:20 +0100

    feat: Add CCO2 settings doctype

--- src/doctypes/index.js
+++ src/doctypes/index.js
@@ -2,6 +2,7 @@ export const APPS_DOCTYPE = 'io.cozy.apps'
 export const FILES_DOCTYPE = 'io.cozy.files'
 export const GEOJSON_DOCTYPE = 'io.cozy.timeseries.geojson'
 export const ACCOUNTS_DOCTYPE = 'io.cozy.accounts'
+export const SETTINGS_DOCTYPE = 'io.cozy.coachco2.settings'
 
 // the documents schema, necessary for CozyClient
 export default {
@@ -12,6 +13,11 @@ export default {
   },
   files: {
     doctype: FILES_DOCTYPE,
+    attributes: {},
+    relationships: {}
+  },
+  settings: {
+    doctype: SETTINGS_DOCTYPE,
     attributes: {},
     relationships: {}
   }
