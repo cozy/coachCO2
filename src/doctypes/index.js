eee9b9e2e4735af403d98fc27998ba34bf8f7063
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-03-08 16:14:40 +0100

    fix: Rename todos schema in timeseries in cozy-client schemas

--- src/doctypes/index.js
+++ src/doctypes/index.js
@@ -5,7 +5,7 @@ import { DOCTYPE_GEOJSON } from 'src/constants/const'
 
 // the documents schema, necessary for CozyClient
 export default {
-  todos: {
+  timeseries: {
     doctype: DOCTYPE_GEOJSON,
     attributes: {},
     relationships: {}
