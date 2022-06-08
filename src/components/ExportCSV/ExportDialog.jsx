085f40771fc9d71e7102390baa36f502c4e0d700
Merkur39 <merkkur39@gmail.com>
2022-06-08 11:01:29 +0200

    refactor: Remove useless useEffect

--- src/components/ExportCSV/ExportDialog.jsx
+++ src/components/ExportCSV/ExportDialog.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 import Buttons from 'cozy-ui/transpiled/react/Buttons'
 import Typography from 'cozy-ui/transpiled/react/Typography'
@@ -10,57 +10,56 @@ import useExportTripsToCSV from 'src/hooks/useExportTripsToCSV'
 const ExportDialog = ({ onClose }) => {
   const { t } = useI18n()
   const client = useClient()
-  const [linkToAppFolder, setLinkToAppFolder] = useState('')
-
   const { file, appDir, isLoading } = useExportTripsToCSV()
 
-  useEffect(() => {
-    if (!isLoading) {
-      const link = generateWebLink({
-        slug: 'drive',
-        cozyUrl: client.getStackClient().uri,
-        subDomainType: client.getInstanceOptions().subdomain,
-        pathname: '/',
-        hash: `folder/${appDir._id}`
-      })
-      setLinkToAppFolder(link)
-    }
-  }, [appDir, client, isLoading])
+  if (isLoading) {
+    return (
+      <ConfirmDialog
+        open
+        title={t('export.modal.title')}
+        content={
+          <Typography color="textPrimary">
+            {t('export.modal.content.progress')}
+          </Typography>
+        }
+      />
+    )
+  }
+
+  const linkToAppFolder = generateWebLink({
+    slug: 'drive',
+    cozyUrl: client.getStackClient().uri,
+    subDomainType: client.getInstanceOptions().subdomain,
+    pathname: '/',
+    hash: `folder/${appDir._id}`
+  })
 
   return (
     <ConfirmDialog
       open
-      onClose={appDir ? onClose : undefined}
+      onClose={onClose}
       title={t('export.modal.title')}
       content={
-        appDir ? (
-          <Typography color="textPrimary">
-            {t('export.modal.content.done', {
-              filename: file.name,
-              pathAppDir: appDir.path
-            })}
-          </Typography>
-        ) : (
-          <Typography color="textPrimary">
-            {t('export.modal.content.progress')}
-          </Typography>
-        )
+        <Typography color="textPrimary">
+          {t('export.modal.content.done', {
+            filename: file.name,
+            pathAppDir: appDir.path
+          })}
+        </Typography>
       }
       actions={
-        appDir && (
-          <>
-            <Buttons
-              variant="secondary"
-              onClick={onClose}
-              label={t('export.modal.button.close')}
-            />
-            <Buttons
-              href={linkToAppFolder}
-              target="_blank"
-              label={t('export.modal.button.viewInDrive')}
-            />
-          </>
-        )
+        <>
+          <Buttons
+            variant="secondary"
+            onClick={onClose}
+            label={t('export.modal.button.close')}
+          />
+          <Buttons
+            href={linkToAppFolder}
+            target="_blank"
+            label={t('export.modal.button.viewInDrive')}
+          />
+        </>
       }
     />
   )
