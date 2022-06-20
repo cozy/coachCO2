4f581078abd6c140d3fa1d2a0fbd74b89c07665e
Paul Tran-Van <paul@cozycloud.cc>
2022-06-20 15:36:02 +0200

    feat: Upgrade cozy-client to get latest DACC methods
    This requires to change the existing remote-doctypes

--- src/doctypes/index.js
+++ src/doctypes/index.js
@@ -5,8 +5,8 @@ export const ACCOUNTS_DOCTYPE = 'io.cozy.accounts'
 export const SETTINGS_DOCTYPE = 'io.cozy.coachco2.settings'
 export const JOBS_DOCTYPE = 'io.cozy.jobs'
 
-export const DACC_REMOTE_DOCTYPE = 'cc.cozycloud.dacc'
-export const DACC_REMOTE_DOCTYPE_DEV = 'cc.cozycloud.dacc.dev'
+export const DACC_REMOTE_DOCTYPE = 'cc.cozycloud.dacc_v2'
+export const DACC_REMOTE_DOCTYPE_DEV = 'cc.cozycloud.dacc.dev_v2'
 
 // the documents schema, necessary for CozyClient
 export default {
