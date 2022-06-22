f48159bb0365181a40989bccaff18ab557df8236
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-06-22 16:42:30 +0200

    feat: Add switch in settings to control DACC alerter
    only if coachco2.admin-mode flag is activated

---
+++ src/components/DaccAlterterSwitcher.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
+import Switch from 'cozy-ui/transpiled/react/MuiCozyTheme/Switch'
+import FormControlLabel from 'cozy-ui/transpiled/react/FormControlLabel'
+
+import useSettings from 'src/hooks/useSettings'
+
+const DaccAlterterSwitcher = props => {
+  const { t } = useI18n()
+  const { isLoading, value, save } = useSettings('hideDaccAlerter')
+
+  const handleChange = ev => {
+    save(ev.target.checked)
+  }
+
+  return (
+    <div {...props}>
+      <FormControlLabel
+        className="u-ml-0"
+        label={t('dacc.settings.hideAlerter')}
+        labelPlacement="start"
+        checked={value}
+        disabled={isLoading}
+        onChange={handleChange}
+        control={<Switch color="primary" />}
+      />
+    </div>
+  )
+}
+
+export default DaccAlterterSwitcher
--- src/components/Views/Settings.jsx
+++ src/components/Views/Settings.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import flag from 'cozy-flags'
 import Spinner from 'cozy-ui/transpiled/react/Spinner'
 
 import {
@@ -10,6 +11,7 @@ import Titlebar from 'src/components/Titlebar'
 import CsvExporter from 'src/components/ExportCSV/CsvExporter'
 import AccountSelector from 'src/components/AccountSelector'
 import DaccSwitcher from 'src/components/DaccSwitcher'
+import DaccAlterterSwitcher from 'src/components/DaccAlterterSwitcher'
 
 export const Settings = () => {
   const { account } = useAccountContext()
@@ -26,6 +28,9 @@ export const Settings = () => {
       <div className="u-mh-1 u-mv-1-half">
         <AccountSelector />
         <DaccSwitcher className="u-mt-1-half" />
+        {flag('coachco2.admin-mode') && (
+          <DaccAlterterSwitcher className="u-mt-1-half" />
+        )}
         <CsvExporter
           className="u-mt-1-half"
           accountName={getAccountLabel(account)}
