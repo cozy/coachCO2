5f30cd889bacc68501e3a229b85d68bfb8d249d6
Paul Tran-Van <paul@cozycloud.cc>
2022-06-27 19:05:58 +0200

    feat: Update purpose in aggregation after edit

--- src/components/EditDialogs/PurposeEditDialog/helpers.spec.js
+++ src/components/EditDialogs/PurposeEditDialog/helpers.spec.js
@@ -1,30 +1,27 @@
 import { createGeojsonWithModifiedPurpose } from './helpers'
 
-const timeserie = {
-  series: [
-    {
-      type: 'FeatureCollection',
-      id: 'otherId',
-      features: [],
-      properties: { manual_purpose: 'HOME' }
-    },
-    {
-      type: 'FeatureCollection',
-      id: 'tripId',
-      features: [],
-      properties: { manual_purpose: 'WORK' }
-    },
-    {
-      type: 'FeatureCollection',
-      id: 'tripWithNoManualPurpose',
-      features: [],
-      properties: {}
-    }
-  ]
+const buildTimeserieWithPurpose = purpose => {
+  const timeserie = {
+    aggregation: {},
+    series: [
+      {
+        type: 'FeatureCollection',
+        id: 'tripId',
+        features: [],
+        properties: {}
+      }
+    ]
+  }
+  if (purpose) {
+    timeserie.series[0].properties = { manual_purpose: purpose }
+    timeserie.aggregation.purpose = purpose
+  }
+  return timeserie
 }
 
 describe('createGeojsonWithModifiedPurpose', () => {
   it('should return the modified timeserie', () => {
+    const timeserie = buildTimeserieWithPurpose('WORK')
     const modifiedTimeserie = createGeojsonWithModifiedPurpose({
       timeserie,
       tripId: 'tripId',
@@ -32,28 +29,29 @@ describe('createGeojsonWithModifiedPurpose', () => {
     })
 
     // to be sure initial timeserie is not mutated
-    expect(timeserie.series[1].properties.manual_purpose).toBe('WORK')
-    // to be sure other series are not impacted
-    expect(timeserie.series[0].properties.manual_purpose).toBe('HOME')
+    expect(timeserie.series[0].properties.manual_purpose).toBe('WORK')
 
-    expect(modifiedTimeserie.series[1].properties.manual_purpose).toBe(
+    expect(modifiedTimeserie.series[0].properties.manual_purpose).toBe(
       'SHOPPING'
     )
+    expect(modifiedTimeserie.aggregation.purpose).toBe('SHOPPING')
   })
 
   it("should create the manual_purpose if it doesn't exist", () => {
+    const timeserie = buildTimeserieWithPurpose()
     const modifiedTimeserie = createGeojsonWithModifiedPurpose({
       timeserie,
-      tripId: 'tripWithNoManualPurpose',
+      tripId: 'tripId',
       purpose: 'SHOPPING'
     })
 
-    expect(modifiedTimeserie.series[2].properties.manual_purpose).toBe(
+    expect(modifiedTimeserie.series[0].properties.manual_purpose).toBe(
       'SHOPPING'
     )
   })
 
   it("should returns a not modified timeserie if tripId doesn't exist", () => {
+    const timeserie = buildTimeserieWithPurpose()
     const modifiedTimeserie = createGeojsonWithModifiedPurpose({
       timeserie,
       tripId: 'idNotFound',
@@ -64,13 +62,14 @@ describe('createGeojsonWithModifiedPurpose', () => {
   })
 
   it('should create the purpose in upper case', () => {
+    const timeserie = buildTimeserieWithPurpose()
     const modifiedTimeserie = createGeojsonWithModifiedPurpose({
       timeserie,
       tripId: 'tripId',
       purpose: 'shopping'
     })
 
-    expect(modifiedTimeserie.series[1].properties.manual_purpose).toBe(
+    expect(modifiedTimeserie.series[0].properties.manual_purpose).toBe(
       'SHOPPING'
     )
   })
