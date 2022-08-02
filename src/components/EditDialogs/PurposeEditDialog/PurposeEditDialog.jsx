8a1062339853f65266ee31325bf60ad4babb6f88
paultranvan <paul@cozycloud.cc>
2022-08-02 16:52:09 +0200

    feat: Ask user for recurring trip
    When the user wants to edit the purpose of a trip, a modal is now used
    to know if the trip is recurring or not. If this is the case, the
    recurring service is started.

--- src/components/EditDialogs/PurposeEditDialog/PurposeEditDialog.jsx
+++ src/components/EditDialogs/PurposeEditDialog/PurposeEditDialog.jsx
@@ -1,16 +1,21 @@
-import React, { useCallback, useMemo } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 
 import { useClient } from 'cozy-client'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import NestedSelectModal from 'cozy-ui/transpiled/react/NestedSelect/Modal'
 
 import { purposes } from 'src/components/helpers'
-import { createGeojsonWithModifiedPurpose } from 'src/components/EditDialogs/PurposeEditDialog/helpers'
 import { PurposeAvatar } from 'src/components/Avatar'
 import { useTrip } from 'src/components/Providers/TripProvider'
 import { OTHER_PURPOSE, RECURRING_PURPOSES_SERVICE_NAME } from 'src/constants'
-import { getGeoJSONData, getTimeseriePurpose } from 'src/lib/timeseries'
+import {
+  getTimeseriePurpose,
+  setAutomaticPurpose,
+  setManualPurpose
+} from 'src/lib/timeseries'
 import { startService } from 'src/lib/services'
+import { ConfirmDialog } from 'cozy-ui/transpiled/react/CozyDialogs'
+import { Button } from 'cozy-ui/transpiled/react/Button'
 
 const makeOptions = t => {
   const options = purposes.map(purpose => ({
@@ -18,35 +23,67 @@ const makeOptions = t => {
     title: t(`trips.purposes.${purpose}`),
     icon: <PurposeAvatar attribute={purpose} />
   }))
-
   return { children: options }
 }
 
 const PurposeEditDialog = ({ onClose }) => {
+  const [showRecurringDialog, setShowRecurringDialog] = useState(false)
+  const [selectedPurpose, setSelectedPurpose] = useState(null)
   const { t } = useI18n()
   const client = useClient()
   const { timeserie } = useTrip()
 
   const handleSelect = useCallback(
     async item => {
+      setSelectedPurpose(item.id)
       const oldPurpose = getTimeseriePurpose(timeserie)
-      const newTimeserie = createGeojsonWithModifiedPurpose({
-        timeserie,
-        tripId: getGeoJSONData(timeserie).id,
-        purpose: item.id
-      })
-      await client.save(newTimeserie)
-      // Start service to set the purpose to similar trips
-      startService(client, RECURRING_PURPOSES_SERVICE_NAME, {
-        fields: {
-          docId: newTimeserie._id,
-          oldPurpose
-        }
-      })
+      if (oldPurpose !== OTHER_PURPOSE) {
+        setShowRecurringDialog(true)
+        return
+      }
+      handleRecurringTrip({ purpose: item.id, oldPurpose })
+
       onClose()
     },
     [client, timeserie, onClose]
   )
+
+  const saveTripWithPurpose = async ({ purpose, isRecurringTrip } = {}) => {
+    let newTimeserie = { ...timeserie }
+    if (isRecurringTrip) {
+      newTimeserie = setAutomaticPurpose(newTimeserie, purpose, {
+        isRecurringTrip
+      })
+    }
+    newTimeserie = setManualPurpose(newTimeserie, purpose, {
+      isRecurringTrip
+    })
+    await client.save(newTimeserie)
+  }
+
+  const handleOccasionalTrip = async () => {
+    await saveTripWithPurpose({
+      purpose: selectedPurpose,
+      isRecurringTrip: false
+    })
+    onClose()
+  }
+
+  const handleRecurringTrip = async ({ purpose, oldPurpose } = {}) => {
+    await saveTripWithPurpose({
+      purpose: purpose || selectedPurpose,
+      isRecurringTrip: true
+    })
+
+    // Start service to set the purpose to similar trips
+    startService(client, RECURRING_PURPOSES_SERVICE_NAME, {
+      fields: {
+        docId: timeserie._id,
+        oldPurpose: oldPurpose || getTimeseriePurpose(timeserie)
+      }
+    })
+    onClose()
+  }
 
   const isSelected = useMemo(
     () => item => {
@@ -58,6 +95,30 @@ const PurposeEditDialog = ({ onClose }) => {
     [timeserie]
   )
 
+  if (showRecurringDialog) {
+    return (
+      <ConfirmDialog
+        open={showRecurringDialog}
+        onClose={onClose}
+        title={t('recurring.confirmDialog.title')}
+        content={t('recurring.confirmDialog.content')}
+        actions={
+          <>
+            <Button
+              theme="secondary"
+              label={t('recurring.confirmDialog.decline')}
+              onClick={handleOccasionalTrip}
+            />
+            <Button
+              theme="primary"
+              label={t('recurring.confirmDialog.confirm')}
+              onClick={handleRecurringTrip}
+            />
+          </>
+        }
+      />
+    )
+  }
   return (
     <NestedSelectModal
       title={t('tripEdit.selectPurpose')}
