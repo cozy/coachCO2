a012a158dfc3910ebcf891f3cdaf782e485af011
PolariTOON <36267812+PolariTOON@users.noreply.github.com>
2022-10-13 10:43:55 +0200

    test: Split the test for the bike goal alerter
    Due to it being controlled.

--- src/components/Goals/BikeGoal/BikeGoalAlertSwitcher.spec.jsx
+++ src/components/Goals/BikeGoal/BikeGoalAlertSwitcher.spec.jsx
@@ -1,24 +1,40 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 
+import { useQuery } from 'cozy-client'
+
 import AppLike from 'test/AppLike'
 import { SETTINGS_DOCTYPE } from 'src/doctypes'
 import BikeGoalAlertSwitcher from 'src/components/Goals/BikeGoal/BikeGoalAlertSwitcher'
 
-jest.mock('cozy-client', () => ({
-  ...jest.requireActual('cozy-client'),
-  useQuery: jest.fn(() => ({ data: {} }))
-}))
+jest.mock('cozy-client/dist/hooks/useQuery', () => jest.fn())
+
+const setup = ({
+  queryMock = {
+    fetchStatus: 'loaded',
+    data: []
+  },
+  mockSave = jest.fn()
+} = {}) => {
+  useQuery.mockReturnValue(queryMock)
+
+  return render(
+    <AppLike client={{ save: mockSave }}>
+      <BikeGoalAlertSwitcher />
+    </AppLike>
+  )
+}
 
 describe('BikeGoalAlertSwitcher', () => {
-  it('should call client.save when switch is clicked', () => {
+  it('should call client.save when switch is clicked with no initial value', () => {
     const mockSave = jest.fn()
-    const { getByRole } = render(
-      <AppLike client={{ save: mockSave }}>
-        <BikeGoalAlertSwitcher />
-      </AppLike>
-    )
+    const { getByRole } = setup({
+      mockSave
+    })
+
     const switchBtn = getByRole('checkbox')
+
+    expect(switchBtn.checked).toBe(true)
 
     fireEvent.click(switchBtn)
     expect(mockSave).toBeCalledTimes(1)
@@ -28,13 +44,62 @@ describe('BikeGoalAlertSwitcher', () => {
         showAlert: false
       }
     })
+  })
+
+  it('should call client.save when switch is clicked with a falsy initial value', () => {
+    const queryMock = {
+      fetchStatus: 'loaded',
+      data: [
+        {
+          bikeGoal: { showAlert: false }
+        }
+      ]
+    }
+    const mockSave = jest.fn()
+    const { getByRole } = setup({
+      queryMock,
+      mockSave
+    })
+
+    const switchBtn = getByRole('checkbox')
+
+    expect(switchBtn.checked).toBe(false)
 
     fireEvent.click(switchBtn)
-    expect(mockSave).toBeCalledTimes(2)
+    expect(mockSave).toBeCalledTimes(1)
     expect(mockSave).toBeCalledWith({
       _type: SETTINGS_DOCTYPE,
       bikeGoal: {
         showAlert: true
+      }
+    })
+  })
+
+  it('should call client.save when switch is clicked with a truthy initial value', () => {
+    const queryMock = {
+      fetchStatus: 'loaded',
+      data: [
+        {
+          bikeGoal: { showAlert: true }
+        }
+      ]
+    }
+    const mockSave = jest.fn()
+    const { getByRole } = setup({
+      queryMock,
+      mockSave
+    })
+
+    const switchBtn = getByRole('checkbox')
+
+    expect(switchBtn.checked).toBe(true)
+
+    fireEvent.click(switchBtn)
+    expect(mockSave).toBeCalledTimes(1)
+    expect(mockSave).toBeCalledWith({
+      _type: SETTINGS_DOCTYPE,
+      bikeGoal: {
+        showAlert: false
       }
     })
   })
