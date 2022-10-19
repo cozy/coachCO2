d90f535a0cd0ef135e055a04dd48351cca837cee
PolariTOON <36267812+PolariTOON@users.noreply.github.com>
2022-10-19 17:33:45 +0200

    fix: Use `useSettings` hook to manage CO2 emission DACC permissions

--- src/components/DaccManager/CO2EmissionDaccManager.jsx
+++ src/components/DaccManager/CO2EmissionDaccManager.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react'
 
-import { isQueryLoading, useClient, useQuery } from 'cozy-client'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 
-import { SETTINGS_DOCTYPE } from 'src/doctypes'
-import { buildSettingsQuery } from 'src/queries/queries'
+import useSettings from 'src/hooks/useSettings'
 import CO2EmissionDaccBanner from 'src/components/DaccBanner/CO2EmissionDaccBanner'
 import CO2EmissionDaccCompareDialog from 'src/components/DaccManager/CO2EmissionDaccCompareDialog'
 import DaccManager from 'src/components/DaccManager/DaccManager'
@@ -13,43 +11,30 @@ const CO2EmissionDaccManager = () => {
   const { t } = useI18n()
   const [showCompareDialog, setShowCompareDialog] = useState(false)
   const [showPermissionsDialog, setShowPermissionsDialog] = useState(false)
-  const client = useClient()
-  const settingsQuery = buildSettingsQuery()
-  const { data: settings, ...settingsQueryLeft } = useQuery(
-    settingsQuery.definition,
-    settingsQuery.options
-  )
-
-  const isLoading = isQueryLoading(settingsQueryLeft)
+  const {
+    isLoading,
+    value: CO2Emission = {},
+    save: setCO2Emission
+  } = useSettings('CO2Emission')
 
   if (isLoading) {
     return null
   }
 
-  const CO2Emission = settings[0]?.CO2Emission || {}
-
   const { showAlert = true, sendToDacc = false } = CO2Emission
 
   const handleOnDiscard = () => {
-    client.save({
-      ...settings[0],
-      _type: SETTINGS_DOCTYPE,
-      CO2Emission: {
-        ...CO2Emission,
-        showAlert: false
-      }
+    setCO2Emission({
+      ...CO2Emission,
+      showAlert: false
     })
   }
 
   const handleOnAccept = () => {
-    client.save({
-      ...settings[0],
-      _type: SETTINGS_DOCTYPE,
-      CO2Emission: {
-        ...CO2Emission,
-        showAlert: false,
-        sendToDACC: true
-      }
+    setCO2Emission({
+      ...CO2Emission,
+      showAlert: false,
+      sendToDACC: true
     })
   }
 
