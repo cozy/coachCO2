bb4d08220e112f3a057ac94173ecac98184a1fdc
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-11-03 10:17:26 +0100

    feat(BikeGoalDateSelector): Relies on year in url, not on date from provider

---
+++ src/components/Goals/BikeGoal/DateSelector/helpers.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+
+// TODO: remove eslint-disable by convert 'src/lib/timeseries' in TS
+// or at least getEarliestTimeserie
+
+import type { TimeSeries } from 'cozy-client/types/models/timeseries'
+
+import differenceInCalendarYears from 'date-fns/differenceInCalendarYears'
+import subYears from 'date-fns/subYears'
+
+import { getEarliestTimeserie } from 'src/lib/timeseries'
+
+export const makeMenuDates = (timeseries: Array<TimeSeries>): Array<string> => {
+  const latestDate = new Date()
+  if (!timeseries || timeseries.length === 0) {
+    return [latestDate.getFullYear().toString()]
+  }
+
+  const earliestDate = new Date(getEarliestTimeserie(timeseries).startDate)
+  const numberOfYears = differenceInCalendarYears(latestDate, earliestDate)
+
+  return Array.from({ length: numberOfYears + 1 }, (_, index) => {
+    return subYears(latestDate, index).getFullYear().toString()
+  })
+}
