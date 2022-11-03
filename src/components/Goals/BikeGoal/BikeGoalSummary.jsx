477d46d8f24879d790048dc2097786f1d2818dda
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-11-03 12:43:05 +0100

    feat(BikeGoal): Use correct query for BikeGoalSummary

--- src/components/Goals/BikeGoal/BikeGoalSummary.jsx
+++ src/components/Goals/BikeGoal/BikeGoalSummary.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { isQueryLoading, useQueryAll } from 'cozy-client'
 import Typography from 'cozy-ui/transpiled/react/Typography'
 import Paper from 'cozy-ui/transpiled/react/Paper'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
@@ -11,24 +12,41 @@ import {
   isGoalCompleted,
   countDaysOrDaysToReach
 } from 'src/components/Goals/BikeGoal/helpers'
-import { useTemporaryQueryForBikeGoal } from 'src/components/Goals/useTemporaryQueryForBikeGoal'
+import { buildBikeCommuteTimeseriesQueryByAccountId } from 'src/queries/queries'
+import { useAccountContext } from 'src/components/Providers/AccountProvider'
+import { filterTimeseriesByYear } from 'src/lib/timeseries'
 
 const BikeGoalSummary = () => {
   const { t } = useI18n()
+  const { account, isAccountLoading } = useAccountContext()
 
-  // TODO: uncomment this when the request return something
-  // const timeseriesQuery =
-  //   buildOneYearBikeCommuteTimeseriesQueryByDateAndAccountId(
-  //     {
-  //       date: new Date(),
-  //       accountId: account?._id
-  //     },
-  //     Boolean(account)
-  //   )
+  const timeseriesQuery = buildBikeCommuteTimeseriesQueryByAccountId(
+    { accountId: account?._id },
+    Boolean(account)
+  )
+  const { data: timeseries, ...timeseriesQueryLeft } = useQueryAll(
+    timeseriesQuery.definition,
+    timeseriesQuery.options
+  )
 
-  // TODO: remove this hooks when the above request will work
-  const { timeseries, isLoadingTimeseriesQuery } =
-    useTemporaryQueryForBikeGoal()
+  const isLoadingTimeseriesQuery =
+    isQueryLoading(timeseriesQueryLeft) || timeseriesQueryLeft.hasMore
+
+  const isLoading = isAccountLoading || isLoadingTimeseriesQuery
+
+  if (isLoading) {
+    return (
+      <Paper
+        elevation={2}
+        className="u-flex u-flex-items-center u-mh-1-s u-mh-2 u-mb-1 u-flex-items-start"
+      >
+        <Spinner size="xlarge" className="u-m-1" />
+      </Paper>
+    )
+  }
+
+  const currentYear = new Date().getFullYear().toString()
+  const timeseriesByYear = filterTimeseriesByYear(timeseries, currentYear)
 
   return (
     <>
@@ -36,33 +54,27 @@ const BikeGoalSummary = () => {
         elevation={2}
         className="u-flex u-flex-items-center u-mh-1-s u-mh-2 u-mb-1 u-flex-items-start"
       >
-        {isLoadingTimeseriesQuery ? (
-          <Spinner size="xlarge" className="u-m-1" />
-        ) : (
-          <>
-            <BikeGoalChart
-              timeseries={timeseries}
-              paddingTop="1rem"
-              paddingLeft="1.5rem"
-              size="small"
-            />
-            <div className="u-ml-1">
-              <Typography variant="h6">{t('bikeGoal.title')}</Typography>
-              <Typography
-                variant="caption"
-                style={{
-                  whiteSpace: 'pre-line',
-                  color: isGoalCompleted(timeseries) && 'var(--successColor)'
-                }}
-              >
-                {t('bikeGoal.goal_progression', {
-                  days: countDaysOrDaysToReach(timeseries),
-                  daysToReach: getDaysToReach()
-                })}
-              </Typography>
-            </div>
-          </>
-        )}
+        <BikeGoalChart
+          timeseries={timeseriesByYear}
+          paddingTop="1rem"
+          paddingLeft="1.5rem"
+          size="small"
+        />
+        <div className="u-ml-1">
+          <Typography variant="h6">{t('bikeGoal.title')}</Typography>
+          <Typography
+            variant="caption"
+            style={{
+              whiteSpace: 'pre-line',
+              color: isGoalCompleted(timeseriesByYear) && 'var(--successColor)'
+            }}
+          >
+            {t('bikeGoal.goal_progression', {
+              days: countDaysOrDaysToReach(timeseriesByYear),
+              daysToReach: getDaysToReach()
+            })}
+          </Typography>
+        </div>
       </Paper>
     </>
   )
