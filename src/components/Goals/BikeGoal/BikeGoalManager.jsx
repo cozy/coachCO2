26fc113dedd5cab9a5533860b451631df324b28b
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-11-15 12:07:09 +0100

    refactor(BikeGoal): Remove usage of useSettings in BikeGoalManager
    We don't want to use a new API to fetch data, sticking to cozy-client API is recommanded

--- src/components/Goals/BikeGoal/BikeGoalManager.jsx
+++ src/components/Goals/BikeGoal/BikeGoalManager.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
-import useSettings from 'src/hooks/useSettings'
-import BikeGoalAlertManager from 'src/components/Goals/BikeGoal/BikeGoalAlertManager'
-import BikeGoalSummary from 'src/components/Goals/BikeGoal/BikeGoalSummary'
-import BikeGoalAlertSuccess from 'src/components/Goals/BikeGoal/BikeGoalAlertSuccess'
-
+import { isQueryLoading, useQuery } from 'cozy-client'
 import Typography from 'cozy-ui/transpiled/react/Typography'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 import Divider from 'cozy-ui/transpiled/react/MuiCozyTheme/Divider'
+
+import { buildSettingsQuery } from 'src/queries/queries'
+import BikeGoalAlertManager from 'src/components/Goals/BikeGoal/BikeGoalAlertManager'
+import BikeGoalSummary from 'src/components/Goals/BikeGoal/BikeGoalSummary'
+import BikeGoalAlertSuccess from 'src/components/Goals/BikeGoal/BikeGoalAlertSuccess'
 
 const style = {
   divider: {
@@ -20,7 +21,19 @@ const style = {
 const BikeGoalManager = () => {
   const { t } = useI18n()
   const { isMobile } = useBreakpoints()
-  const { value: bikeGoal = {} } = useSettings('bikeGoal')
+
+  const settingsQuery = buildSettingsQuery()
+  const { data: settings, ...settingsQueryLeft } = useQuery(
+    settingsQuery.definition,
+    settingsQuery.options
+  )
+  const isLoading = isQueryLoading(settingsQueryLeft)
+
+  if (isLoading) {
+    return null
+  }
+
+  const bikeGoal = settings?.[0]?.bikeGoal ?? {}
   const {
     activated = false,
     showAlert = true,
