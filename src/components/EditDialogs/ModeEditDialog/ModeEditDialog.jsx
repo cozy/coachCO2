f9b1cf317bca142d41e5b3b27506536771a922c4
Alexis G <merkkur39@gmail.com>
2023-09-07 16:45:05 +0200

    feat: Updated `ModeEditDialog` component
    To use the new `options` to manage an action on certain modes of transport, which will allow you to define a default mode in its category

--- src/components/EditDialogs/ModeEditDialog/ModeEditDialog.jsx
+++ src/components/EditDialogs/ModeEditDialog/ModeEditDialog.jsx
@@ -1,39 +1,41 @@
-import React, { useCallback, useMemo } from 'react'
-import { ModeAvatar } from 'src/components/Avatar'
+import React, { useCallback, useMemo, useState } from 'react'
+import DialogDefaultMode from 'src/components/EditDialogs/DialogDefaultMode'
 import { createGeojsonWithModifiedMode } from 'src/components/EditDialogs/ModeEditDialog/helpers'
+import ModeOptionAction from 'src/components/EditDialogs/ModeOptionAction'
+import { makeOptions } from 'src/components/EditDialogs/helpers'
 import { useTrip } from 'src/components/Providers/TripProvider'
-import { modes } from 'src/components/helpers'
+import { buildSettingsQuery } from 'src/queries/queries'
 
-import { useClient } from 'cozy-client'
+import { useClient, useQuery } from 'cozy-client'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import NestedSelectModal from 'cozy-ui/transpiled/react/NestedSelect/Modal'
 
-const makeOptions = t => {
-  const options = modes.map(mode => ({
-    id: mode,
-    title: t(`trips.modes.${mode}`),
-    icon: <ModeAvatar attribute={mode} />
-  }))
-
-  return { children: options }
-}
-
 const ModeEditDialog = ({ section, onClose }) => {
+  const [defaultModeDialog, setDefaultModeDialog] = useState(null)
   const { t } = useI18n()
   const client = useClient()
   const { timeserie } = useTrip()
+  const settingsQuery = buildSettingsQuery()
+  const { data: settings } = useQuery(
+    settingsQuery.definition,
+    settingsQuery.options
+  )
+
+  const appSetting = useMemo(() => settings?.[0] || {}, [settings])
+  const defaultModes = appSetting?.defaultTransportModeByGroup
 
   const handleSelect = useCallback(
     async item => {
       const newTimeserie = createGeojsonWithModifiedMode({
         timeserie,
         sectionId: section.id,
-        mode: item.id
+        mode: item.id,
+        appSetting
       })
       await client.save(newTimeserie)
       onClose()
     },
-    [client, timeserie, onClose, section.id]
+    [client, timeserie, onClose, section.id, appSetting]
   )
 
   const isSelected = useMemo(
@@ -41,14 +43,34 @@ const ModeEditDialog = ({ section, onClose }) => {
     [section.mode]
   )
 
+  const handleActionClick = ({ item }) => {
+    setDefaultModeDialog(item)
+  }
+  const options = makeOptions(t, {
+    defaultModes,
+    action: {
+      Component: ModeOptionAction,
+      props: { onClick: handleActionClick }
+    }
+  })
+
   return (
-    <NestedSelectModal
-      title={t('tripEdit.selectMode')}
-      onClose={onClose}
-      onSelect={handleSelect}
-      isSelected={isSelected}
-      options={makeOptions(t)}
-    />
+    <>
+      <NestedSelectModal
+        title={t('tripEdit.selectMode')}
+        onClose={onClose}
+        onSelect={handleSelect}
+        isSelected={isSelected}
+        options={options}
+      />
+      {!!defaultModeDialog && (
+        <DialogDefaultMode
+          item={defaultModeDialog}
+          onClose={() => setDefaultModeDialog(null)}
+          onConfirm={() => handleSelect(defaultModeDialog)}
+        />
+      )}
+    </>
   )
 }
 
