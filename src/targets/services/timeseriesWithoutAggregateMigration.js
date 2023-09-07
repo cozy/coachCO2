804ee45162d2dd2d2b6925dfc1b1f45be1edb2d9
Alexis G <merkkur39@gmail.com>
2023-09-07 16:40:00 +0200

    feat: Add default mode to `getFeatureMode` helper
    The default mode must have priority over the sensed mode,
    but the manual mode remains priority over all.

--- src/targets/services/timeseriesWithoutAggregateMigration.js
+++ src/targets/services/timeseriesWithoutAggregateMigration.js
@@ -1,7 +1,10 @@
 import fetch from 'node-fetch'
 import schema from 'src/doctypes'
 import { computeAggregatedTimeseries } from 'src/lib/timeseries'
-import { buildTimeseriesWithoutAggregation } from 'src/queries/queries'
+import {
+  buildSettingsQuery,
+  buildTimeseriesWithoutAggregation
+} from 'src/queries/queries'
 
 import CozyClient from 'cozy-client'
 import log from 'cozy-logger'
@@ -13,10 +16,18 @@ const migrateTimeSeriesWithoutAggregation = async () => {
   log('info', `Start migrateTimeSeriesWithoutAggregation service`)
   const client = CozyClient.fromEnv(process.env, { schema })
 
-  const query = buildTimeseriesWithoutAggregation({
-    limit: BATCH_DOCS_LIMIT
-  }).definition
-  const resp = await client.query(query)
+  const timeseriesWithoutAggregationQueryDef =
+    buildTimeseriesWithoutAggregation({
+      limit: BATCH_DOCS_LIMIT
+    }).definition
+  const settingsQuery = buildSettingsQuery()
+
+  const { data: settings } = await client.query(
+    settingsQuery.definition,
+    settingsQuery.options
+  )
+  const appSetting = settings?.[0] || {}
+  const resp = await client.query(timeseriesWithoutAggregationQueryDef)
 
   if (!resp.data || resp.data.length < 1) {
     log('info', 'Nothing to migrate')
@@ -25,7 +36,7 @@ const migrateTimeSeriesWithoutAggregation = async () => {
   log('info', `Found ${resp.data.length} timeseries to migrate...`)
 
   // Compute aggregation for all retrieved timeseries
-  const migratedTimeseries = computeAggregatedTimeseries(resp.data)
+  const migratedTimeseries = computeAggregatedTimeseries(resp.data, appSetting)
 
   // Save the migrated timeseries
   for (const timeserie of migratedTimeseries) {
