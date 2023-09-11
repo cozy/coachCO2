5ca726af8b6f6340247da12745059a3e5ca12909
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-09-11 12:16:41 +0200

    feat Add end date in trip dialog

--- src/components/Trip/TripDialogDesktop.jsx
+++ src/components/Trip/TripDialogDesktop.jsx
@@ -2,21 +2,33 @@ import React from 'react'
 import { useNavigate, useLocation, useParams } from 'react-router-dom'
 import { useTrip } from 'src/components/Providers/TripProvider'
 import TripDialogDesktopContent from 'src/components/Trip/TripDialogDesktopContent'
-import { getEndPlaceDisplayName } from 'src/lib/timeseries'
+import { getEndPlaceDisplayName, getEndDate } from 'src/lib/timeseries'
 
 import { Dialog } from 'cozy-ui/transpiled/react/CozyDialogs'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
+import Typography from 'cozy-ui/transpiled/react/Typography'
 
 const TripDialogDesktop = () => {
+  const { f } = useI18n()
   const { timeserieId } = useParams()
   const { timeserie } = useTrip()
   const { pathname } = useLocation()
   const navigate = useNavigate()
 
+  const date = f(getEndDate(timeserie), 'dddd D MMMM YYYY')
+
   return (
     <Dialog
       open
       size="large"
-      title={getEndPlaceDisplayName(timeserie)}
+      title={
+        <>
+          {getEndPlaceDisplayName(timeserie)}
+          <Typography className="u-mt-half" variant="caption">
+            {date}
+          </Typography>
+        </>
+      }
       content={<TripDialogDesktopContent />}
       onClose={() => navigate(pathname.split(`/${timeserieId}`)[0])}
     />
