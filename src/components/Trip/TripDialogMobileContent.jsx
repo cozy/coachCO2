6ccbd017311ef2723b90c329e3ac5c4db3bda7f6
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-10-16 12:12:46 +0200

    fix: Adjust info block style in trip dialog

--- src/components/Trip/TripDialogMobileContent.jsx
+++ src/components/Trip/TripDialogMobileContent.jsx
@@ -6,27 +6,28 @@ import TripMap from 'src/components/Trip/TripMap'
 import BottomSheet, {
   BottomSheetHeader
 } from 'cozy-ui/transpiled/react/BottomSheet'
+import { makeStyles } from 'cozy-ui/transpiled/react/styles'
 
 export const bottomSheetSettings = {
   mediumHeightRatio: 0.66
 }
 
-const makeMapStyles = ({ toolbarHeight }) => ({
+const useStyles = makeStyles({
   mapContainer: {
-    height: `calc(100vh - ${toolbarHeight}px - var(--sidebarHeight) - env(safe-area-inset-bottom))`
+    height: ({ toolbarHeight }) =>
+      `calc(100vh - ${toolbarHeight}px - var(--sidebarHeight) - env(safe-area-inset-bottom))`
+  },
+  bottomSheetHeader: {
+    gap: '0.5rem'
   }
 })
 
 const TripDialogMobileContent = ({ titleRef }) => {
   const [toolbarHeight, setToolbarHeight] = useState(0)
+  const styles = useStyles({ toolbarHeight })
 
   const toolbarProps = useMemo(
     () => ({ height: toolbarHeight }),
-    [toolbarHeight]
-  )
-
-  const styles = useMemo(
-    () => makeMapStyles({ toolbarHeight: toolbarHeight }),
     [toolbarHeight]
   )
 
@@ -38,11 +39,13 @@ const TripDialogMobileContent = ({ titleRef }) => {
 
   return (
     <>
-      <div className="u-w-100 u-pos-fixed" style={styles.mapContainer}>
+      <div className={`u-w-100 u-pos-fixed ${styles.mapContainer}`}>
         <TripMap />
       </div>
       <BottomSheet settings={bottomSheetSettings} toolbarProps={toolbarProps}>
-        <BottomSheetHeader className="u-h-3 u-pb-half">
+        <BottomSheetHeader
+          className={`u-h-3 u-pb-half u-ph-half ${styles.bottomSheetHeader}`}
+        >
           <BottomSheetHeaderContent />
         </BottomSheetHeader>
         <BottomSheetContent />
