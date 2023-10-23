8c0091de861560a232314baad3729c276943e857
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-10-23 14:29:01 +0200

    refactor: Move titlebar from SpinnerOrEmpty to EmptyContent

--- src/components/EmptyContent.jsx
+++ src/components/EmptyContent.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import EmptySvg from 'src/assets/icons/empty.svg'
+import Titlebar from 'src/components/Titlebar'
 
+import { useClient } from 'cozy-client'
 import Empty from 'cozy-ui/transpiled/react/Empty'
 import useBreakpoints from 'cozy-ui/transpiled/react/providers/Breakpoints'
 import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
@@ -8,16 +10,20 @@ import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 const EmptyContent = () => {
   const { t } = useI18n()
   const { isMobile } = useBreakpoints()
+  const client = useClient()
 
   return (
-    <Empty
-      data-testid="EmptyContent"
-      className={isMobile ? 'u-p-1' : 'u-flex-justify-start u-p-3'}
-      icon={EmptySvg}
-      iconSize="large"
-      title={t('empty.title')}
-      text={t('empty.text')}
-    />
+    <>
+      {isMobile && <Titlebar label={client.appMetadata.slug} />}
+      <Empty
+        data-testid="EmptyContent"
+        className={isMobile ? 'u-p-1' : 'u-flex-justify-start u-p-3'}
+        icon={EmptySvg}
+        iconSize="large"
+        title={t('empty.title')}
+        text={t('empty.text')}
+      />
+    </>
   )
 }
 
