c831d4dd1cf35b424fbb20dca5410ce04a80d414
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-10-25 11:34:33 +0200

    feat: Add 3 views to show specific content when no data

---
+++ src/components/EmptyContent/ChangeAccount.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import EmptySvg from 'src/assets/icons/pins-path.svg'
+import {
+  useAccountContext,
+  getAccountLabel
+} from 'src/components/Providers/AccountProvider'
+import Titlebar from 'src/components/Titlebar'
+
+import Button from 'cozy-ui/transpiled/react/Buttons'
+import Empty from 'cozy-ui/transpiled/react/Empty'
+import useBreakpoints from 'cozy-ui/transpiled/react/providers/Breakpoints'
+import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
+
+const ChangeAccount = () => {
+  const { t } = useI18n()
+  const { isMobile } = useBreakpoints()
+  const navigate = useNavigate()
+  const { account } = useAccountContext()
+
+  return (
+    <>
+      {isMobile && (
+        <Titlebar label={t('trips.from') + ' ' + getAccountLabel(account)} />
+      )}
+      <Empty
+        icon={EmptySvg}
+        title={t('emptyContent.changeAccount.title')}
+        text={t('emptyContent.changeAccount.text')}
+        centered
+      >
+        <Button
+          className="u-mt-1"
+          variant="secondary"
+          label={t('emptyContent.changeAccount.action')}
+          onClick={() => navigate('/settings')}
+        />
+      </Empty>
+    </>
+  )
+}
+
+export default ChangeAccount
