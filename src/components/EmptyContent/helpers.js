8733b39d63d702f617a2fbec71d5f9df034cf533
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-11-06 16:11:20 +0100

    feat: Adapt welcome text according to flags

--- src/components/EmptyContent/helpers.js
+++ src/components/EmptyContent/helpers.js
@@ -1,5 +1,7 @@
 import { buildHasTimeseriesQueryByAccountId } from 'src/queries/queries'
 
+import flag from 'cozy-flags'
+
 export const makeQueriesByAccountsId = accounts => {
   const accountsIds = accounts.map(account => account._id)
 
@@ -10,4 +12,46 @@ export const makeQueriesByAccountsId = accounts => {
   }, {})
 
   return queriesByAccountsId
+}
+
+export const makeWelcomeText = () => {
+  const maxDaysToCapture = flag('coachco2.max-days-to-capture')
+  const isMaxDaysToCapturePositive =
+    typeof maxDaysToCapture === 'number' && maxDaysToCapture >= 0
+  const isMaxDaysToCaptureNegative =
+    typeof maxDaysToCapture === 'number' && maxDaysToCapture < 0
+  const isBikeGoalEnabled = flag('coachco2.bikegoal.enabled') === true
+  const sourceOffer = flag('coachco2.bikegoal.settings')?.sourceOffer
+  const sourceName = flag('coachco2.bikegoal.settings')?.sourceName
+
+  if (isBikeGoalEnabled && isMaxDaysToCapturePositive) {
+    return 'textA'
+  }
+
+  if (
+    isBikeGoalEnabled &&
+    isMaxDaysToCaptureNegative &&
+    (!sourceOffer || sourceOffer === 'premium')
+  ) {
+    return 'textB'
+  }
+
+  if (
+    isBikeGoalEnabled &&
+    isMaxDaysToCaptureNegative &&
+    sourceOffer !== 'employer' &&
+    !!sourceName
+  ) {
+    return 'textC'
+  }
+
+  if (
+    isBikeGoalEnabled &&
+    isMaxDaysToCaptureNegative &&
+    sourceOffer === 'employer'
+  ) {
+    return 'textD'
+  }
+
+  return 'text'
 }
--- src/components/EmptyContent/helpers.spec.js
+++ src/components/EmptyContent/helpers.spec.js
@@ -1,4 +1,13 @@
-import { makeQueriesByAccountsId } from 'src/components/EmptyContent/helpers.js'
+import {
+  makeQueriesByAccountsId,
+  makeWelcomeText
+} from 'src/components/EmptyContent/helpers.js'
+
+import flag from 'cozy-flags'
+
+jest.mock('cozy-flags')
+
+const setupFlags = flags => flag.mockImplementation(flagName => flags[flagName])
 
 describe('makeQueriesByAccountsId', () => {
   it('should create well formatted object', () => {
@@ -16,5 +25,142 @@ describe('makeQueriesByAccountsId', () => {
         query: expect.any(Object)
       }
     })
+  })
+})
+
+describe('makeWelcomeText', () => {
+  it('should return text for maxDaysToCapture undefined', () => {
+    setupFlags({
+      'coachco2.max-days-to-capture': undefined,
+      'coachco2.bikegoal.enabled': true
+    })
+
+    expect(makeWelcomeText()).toBe('text')
+  })
+
+  it('should return text for maxDaysToCapture undefined even if bikegoal disabled', () => {
+    setupFlags({
+      'coachco2.max-days-to-capture': undefined,
+      'coachco2.bikegoal.enabled': false
+    })
+
+    expect(makeWelcomeText()).toBe('text')
+  })
+
+  it('should return text for maxDaysToCapture undefined even if bikegoal undefined', () => {
+    setupFlags({
+      'coachco2.max-days-to-capture': undefined,
+      'coachco2.bikegoal.enabled': undefined
+    })
+
+    expect(makeWelcomeText()).toBe('text')
+  })
+
+  it('should return text for maxDaysToCapture null', () => {
+    setupFlags({
+      'coachco2.max-days-to-capture': null,
+      'coachco2.bikegoal.enabled': true
+    })
+
+    expect(makeWelcomeText()).toBe('text')
+  })
+
+  it('should return text for B2C without option, premium or B2B', () => {
+    setupFlags({
+      'coachco2.max-days-to-capture': 10,
+      'coachco2.bikegoal.enabled': true
+    })
+
+    expect(makeWelcomeText()).toBe('textA')
+  })
+
+  it('should return text for B2C without option, even for premium', () => {
+    setupFlags({
+      'coachco2.max-days-to-capture': 10,
+      'coachco2.bikegoal.enabled': true,
+      'coachco2.bikegoal.settings': { sourceOffer: 'premium' }
+    })
+
+    expect(makeWelcomeText()).toBe('textA')
+  })
+
+  it('should return text for B2C with premium', () => {
+    setupFlags({
+      'coachco2.max-days-to-capture': -1,
+      'coachco2.bikegoal.enabled': true,
+      'coachco2.bikegoal.settings': { sourceOffer: 'premium' }
+    })
+
+    expect(makeWelcomeText()).toBe('textB')
+  })
+
+  it('should return text for B2C with premium even without sourceOffer', () => {
+    setupFlags({
+      'coachco2.max-days-to-capture': -1,
+      'coachco2.bikegoal.enabled': true,
+      'coachco2.bikegoal.settings': { sourceOffer: undefined }
+    })
+
+    expect(makeWelcomeText()).toBe('textB')
+  })
+
+  it('should return text for B2C with custom sourceOffer ', () => {
+    setupFlags({
+      'coachco2.max-days-to-capture': -1,
+      'coachco2.bikegoal.enabled': true,
+      'coachco2.bikegoal.settings': { sourceOffer: 'other' }
+    })
+
+    expect(makeWelcomeText()).toBe('text')
+  })
+
+  it('should return text for B2B sponsored to constituents', () => {
+    setupFlags({
+      'coachco2.max-days-to-capture': -1,
+      'coachco2.bikegoal.enabled': true,
+      'coachco2.bikegoal.settings': {
+        sourceOffer: 'otherThanEmployer',
+        sourceName: 'notEmpty'
+      }
+    })
+
+    expect(makeWelcomeText()).toBe('textC')
+  })
+
+  it('should return text for other B2B', () => {
+    setupFlags({
+      'coachco2.max-days-to-capture': -1,
+      'coachco2.bikegoal.enabled': true,
+      'coachco2.bikegoal.settings': {
+        sourceOffer: 'otherThanEmployer'
+      }
+    })
+
+    expect(makeWelcomeText()).toBe('text')
+  })
+
+  it('should return text for B2B sponsored to employees', () => {
+    setupFlags({
+      'coachco2.max-days-to-capture': -1,
+      'coachco2.bikegoal.enabled': true,
+      'coachco2.bikegoal.settings': {
+        sourceOffer: 'employer'
+      }
+    })
+
+    expect(makeWelcomeText()).toBe('textD')
+  })
+
+  it('should return text for B2B sponsored to employees even with sourceName', () => {
+    setupFlags({
+      'coachco2.max-days-to-capture': -1,
+      'coachco2.bikegoal.enabled': true,
+      'coachco2.bikegoal.settings': {
+        sourceOffer: 'employer',
+        sourceName: 'notEmpty'
+      }
+    })
+
+    expect(makeWelcomeText()).toBe('textD')
   })
 })
