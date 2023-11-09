c64ca09f45c8555cf06277a908cdec99962a559e
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-11-09 16:23:39 +0100

    feat: Add empty content for BikeGoal page

--- src/components/Goals/BikeGoal/BikeGoalList.jsx
+++ src/components/Goals/BikeGoal/BikeGoalList.jsx
@@ -3,10 +3,19 @@ import React from 'react'
 import TripsList from 'src/components/TripsList'
 
 import LoadMore from 'cozy-ui/transpiled/react/LoadMore'
+import Typography from 'cozy-ui/transpiled/react/Typography'
 import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
 const BikeGoalList = ({ className, timeseries, hasMore, fetchMore }) => {
   const { t } = useI18n()
+
+  if (timeseries?.length === 0) {
+    return (
+      <Typography className="u-mt-2 u-p-2" variant="h5" align="center">
+        {t('bikeGoal.empty.title')}
+      </Typography>
+    )
+  }
 
   return (
     <div className={className}>
