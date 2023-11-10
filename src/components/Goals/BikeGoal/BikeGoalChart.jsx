63f79218aa753b3438b024edc4d8aad92c265dbd
paultranvan <paul@cozycloud.cc>
2023-11-10 16:40:35 +0100

    feat: Use actual DACC values for BikeGoalChart component

--- src/components/Goals/BikeGoal/BikeGoalChart.jsx
+++ src/components/Goals/BikeGoal/BikeGoalChart.jsx
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   makeGoalAchievementPercentage,
   makeIconSize,
   makeDaccAchievementPercentage
 } from 'src/components/Goals/BikeGoal/helpers'
+import { DACC_MEASURE_NAME_BIKE_GOAL } from 'src/constants'
+import useFetchDACCAggregates from 'src/hooks/useFetchDACCAggregates'
 import { buildSettingsQuery } from 'src/queries/queries'
 
 import { isQueryLoading, useQuery } from 'cozy-client'
@@ -12,12 +14,22 @@ import Box from 'cozy-ui/transpiled/react/Box'
 import CircularChart from 'cozy-ui/transpiled/react/CircularChart'
 
 const BikeGoalChart = ({ timeseries, sendToDACC, size, ...props }) => {
+  const [hasDACCConsent, setHasDACCConsent] = useState(false)
   const settingsQuery = buildSettingsQuery()
   const { data: settings, ...settingsQueryLeft } = useQuery(
     settingsQuery.definition,
     settingsQuery.options
   )
-  const isLoading = isQueryLoading(settingsQueryLeft)
+  useEffect(() => {
+    const consent = settings?.[0].bikeGoal?.sendToDACC
+    setHasDACCConsent(!!consent)
+  }, [settings])
+
+  const { data: avgDays, isLoading: isDACCLoading } = useFetchDACCAggregates({
+    hasConsent: hasDACCConsent,
+    measureName: DACC_MEASURE_NAME_BIKE_GOAL
+  })
+  const isLoading = isQueryLoading(settingsQueryLeft) && isDACCLoading
 
   if (isLoading) {
     return null
@@ -35,7 +47,7 @@ const BikeGoalChart = ({ timeseries, sendToDACC, size, ...props }) => {
       primaryProps={{ value: goalAchievementPercentage, color: '#BA5AE8' }}
       {...(sendToDACC && {
         secondaryProps: {
-          value: makeDaccAchievementPercentage(settings),
+          value: makeDaccAchievementPercentage(settings, avgDays),
           color: '#BA5AE83D'
         }
       })}
