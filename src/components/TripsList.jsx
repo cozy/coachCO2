cc3c229447fb6c73a5b9a16f0b881edab88bab68
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-12-18 11:03:37 +0100

    feat: Add tooltip when geoloc is disabled

--- src/components/TripsList.jsx
+++ src/components/TripsList.jsx
@@ -1,9 +1,14 @@
 import isSameDay from 'date-fns/isSameDay'
 import PropTypes from 'prop-types'
 import React, { useCallback } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useGeolocationTracking } from 'src/components/Providers/GeolocationTrackingProvider'
 import TripItem from 'src/components/TripItem'
 import { getStartDate } from 'src/lib/timeseries'
 
+import Alert from 'cozy-ui/transpiled/react/Alert'
+import AlertTitle from 'cozy-ui/transpiled/react/AlertTitle'
+import Button from 'cozy-ui/transpiled/react/Buttons'
 import List from 'cozy-ui/transpiled/react/List'
 import Paper from 'cozy-ui/transpiled/react/Paper'
 import Typography from 'cozy-ui/transpiled/react/Typography'
@@ -13,6 +18,12 @@ import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 export const TripsList = ({ timeseries, noHeader }) => {
   const { isMobile } = useBreakpoints()
   const { t } = useI18n()
+  const navigate = useNavigate()
+  const { isGeolocationTrackingAvailable, isGeolocationTrackingEnabled } =
+    useGeolocationTracking()
+
+  const showGeolocationTooltip =
+    isGeolocationTrackingAvailable && !isGeolocationTrackingEnabled
 
   const hasDateHeader = useCallback(
     (timeserie, index) => {
@@ -34,6 +45,23 @@ export const TripsList = ({ timeseries, noHeader }) => {
         >
           <Typography variant="h5">{t('trips.trips')}</Typography>
         </Paper>
+      )}
+      {showGeolocationTooltip && (
+        <Alert
+          className="u-mt-half u-mh-half"
+          block
+          action={
+            <Button
+              variant="text"
+              size="small"
+              label={t('geolocationTracking.tooltip.action')}
+              onClick={() => navigate('/settings')}
+            />
+          }
+        >
+          <AlertTitle>{t('geolocationTracking.tooltip.title')}</AlertTitle>
+          {t('geolocationTracking.tooltip.text')}
+        </Alert>
       )}
       <List>
         {timeseries.map((timeserie, index) => (
