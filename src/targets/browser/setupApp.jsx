dd1cddf857637feef345322e5dcd4670a2498e10
Alexis G <merkkur39@gmail.com>
2023-12-20 15:10:14 +0100

    chore: Add Sentry
    To intercept the error logs, you must add the
    `CaptureConsole` integration of Sentry,
    this is necessary because otherwise all the error logs
    in the `try/catch` are invisible.
    
    And added `ignoreErrors`, to avoid polluting Sentry
    with React `warnings` (called with "console.error").

--- src/targets/browser/setupApp.jsx
+++ src/targets/browser/setupApp.jsx
@@ -1,3 +1,5 @@
+import { CaptureConsole } from '@sentry/integrations'
+import * as Sentry from '@sentry/react'
 import memoize from 'lodash/memoize'
 import { getValues, initBar } from 'src/utils/bar'
 import { getClient } from 'src/utils/client'
@@ -5,6 +7,8 @@ import { getClient } from 'src/utils/client'
 import flag from 'cozy-flags'
 import { RealtimePlugin } from 'cozy-realtime'
 import { initTranslation } from 'cozy-ui/transpiled/react/providers/I18n'
+
+import manifest from '../../../manifest.webapp'
 
 /**
  * Memoize this function in its own file so that it is correctly memoized
@@ -16,6 +20,19 @@ const setupApp = memoize(() => {
   const client = getClient()
   client.registerPlugin(flag.plugin)
   client.registerPlugin(RealtimePlugin)
+
+  Sentry.init({
+    dsn: 'https://9f18ae40b7a3ec801af8fcee845bca53@errors.cozycloud.cc/67',
+    environment: process.env.NODE_ENV,
+    release: manifest.version,
+    integrations: [
+      new CaptureConsole({ levels: ['error'] }), // We also want to capture the `console.error` to, among other things, report the logs present in the `try/catch`
+      new Sentry.BrowserTracing()
+    ],
+    tracesSampleRate: 1,
+    // React log these warnings(bad Proptypes), in a console.error, it is not relevant to report this type of information to Sentry
+    ignoreErrors: [/^Warning: /]
+  })
 
   initBar({ client, container, lang, appName })
 
