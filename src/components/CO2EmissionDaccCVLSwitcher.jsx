df2f841f9938e50fe512aed9eacb84040e3f0c8e
Alexis G <merkkur39@gmail.com>
2024-01-10 15:03:05 +0100

    feat: Add an option to settings to enable DACC for CVL context

---
+++ src/components/CO2EmissionDaccCVLSwitcher.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import useSettings from 'src/hooks/useSettings'
+
+import Icon from 'cozy-ui/transpiled/react/Icon'
+import PieChartIcon from 'cozy-ui/transpiled/react/Icons/PieChart'
+import ListItem from 'cozy-ui/transpiled/react/ListItem'
+import ListItemIcon from 'cozy-ui/transpiled/react/ListItemIcon'
+import ListItemSecondaryAction from 'cozy-ui/transpiled/react/ListItemSecondaryAction'
+import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
+import Switch from 'cozy-ui/transpiled/react/Switch'
+import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
+
+const CO2EmissionDaccCVLSwitcher = () => {
+  const { t } = useI18n()
+
+  const {
+    isLoading,
+    value = false,
+    save
+  } = useSettings('centreValDeLoireExpe.sendToDACC')
+
+  const handleChange = val => {
+    const value = val?.target?.checked ?? val
+    save(value)
+  }
+
+  return (
+    <ListItem
+      button
+      ellipsis={false}
+      onClick={() => handleChange(!value)}
+      disabled={isLoading}
+    >
+      <ListItemIcon>
+        <Icon icon={PieChartIcon} />
+      </ListItemIcon>
+      <ListItemText primary={t('centreValDeLoireExpe.settings.sendToDACC')} />
+      <ListItemSecondaryAction>
+        <Switch color="primary" checked={value} onChange={handleChange} />
+      </ListItemSecondaryAction>
+    </ListItem>
+  )
+}
+
+export default CO2EmissionDaccCVLSwitcher
