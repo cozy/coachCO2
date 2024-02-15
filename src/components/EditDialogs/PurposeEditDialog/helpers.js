3ed9a01eedc87b4960b5765fff86cef447a61090
AlexisG <merkkur39@gmail.com>
2024-02-15 16:18:20 +0100

    feat: Behavior of POI modal
    For all changes of purpose, except "Going to or from work",
    it should only appear if no POI has ever been saved.
    
    As soon as a POI is created, the POI modal
    should never be displayed again
    (except "Going to or from work" purpose)

--- src/components/EditDialogs/PurposeEditDialog/helpers.js
+++ src/components/EditDialogs/PurposeEditDialog/helpers.js
@@ -28,19 +28,26 @@ export const createGeojsonWithModifiedPurpose = ({
   return timeserie
 }
 
+/**
+ * @param {Object} params
+ * @param {Object} params.setting - The io.cozy.coachco2.settings document
+ * @param {Object} params.timeserie - The timeserie document
+ * @param {string} params.selectedPurpose - The selected purpose
+ * @param {Function} params.setContactToPlaceType - The function to set the contact to place type
+ * @param {Function} params.onClose - The function to close the dialog
+ */
 export const openContactToPlaceModalOrClose = ({
+  setting,
   timeserie,
   selectedPurpose,
   setContactToPlaceType,
   onClose
 }) => {
+  const hidePoiModal = setting?.hidePoiModal
   const isCommute = selectedPurpose === COMMUTE_PURPOSE
   const isOtherPurpose = selectedPurpose === OTHER_PURPOSE
-  const hasConstactToPlace =
-    hasRelationshipByType(timeserie, 'start') ||
-    hasRelationshipByType(timeserie, 'end')
 
-  if ((!isOtherPurpose && !hasConstactToPlace) || isCommute) {
+  if ((!isOtherPurpose && !hidePoiModal) || isCommute) {
     if (!hasRelationshipByType(timeserie, 'start')) {
       setContactToPlaceType('start')
     } else if (!hasRelationshipByType(timeserie, 'end')) {
